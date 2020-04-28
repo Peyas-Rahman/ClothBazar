8ed36f5f73420cbc3b3bd407cb00f0df8d13b7c2
peyas <peyas16bd@yahoo.com>
2020-04-28 02:36:17 -0500

    Add Database and Business Entities

---
+++ ClothBazar.Database/CBContext.cs
@@ -0,0 +1,16 @@
+﻿using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Database
+{
+    class CBContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
